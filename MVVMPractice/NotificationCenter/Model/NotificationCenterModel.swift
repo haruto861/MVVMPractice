2dfcbcdac83b14aee905b644eb6b19fc3ca89abb
haruto-matsushima <matsu_haruto@icloud.com>
2021-10-18 14:27:04 +0900

    add file

---
+++ MVVMPractice/NotificationCenter/Model/NotificationCenterModel.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationCenterModel.swift
+//  MVVMPractice
+//
+//  Created by 松島悠人 on 2021/10/18.
+//
+
+import Foundation
---
+++ MVVMPractice/NotificationCenter/View/NotificationCenterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationCenterViewController.swift
+//  MVVMPractice
+//
+//  Created by 松島悠人 on 2021/10/18.
+//
+
+import UIKit
+
+class NotificationCenterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
