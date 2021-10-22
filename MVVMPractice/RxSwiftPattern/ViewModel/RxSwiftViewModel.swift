852ac6dba5bbac7c408abb6efd01cb8d5ae16aca
haruto-matsushima <matsu_haruto@icloud.com>
2021-10-22 22:23:36 +0900

    add rx

---
+++ MVVMPractice/RxSwiftPattern/ViewModel/RxSwiftViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RxSwiftViewModel.swift
+//  MVVMPractice
+//
+//  Created by 松島悠人 on 2021/10/22.
+//
+
+import Foundation
+// viewに表示するためのデータを保持する
+// viewからイベントを受け取りmodelの処理を呼び出す
+// viewからイベントを受け取り加工して値を更新する
+final class RxSwiftViewModel {
+}
